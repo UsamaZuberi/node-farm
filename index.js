3e20e5fa5b831e118a1e7c817cc55336044bb8f8
UsamaZuberi <muhammad.usama@va8ivedigital.com>
2021-12-24 09:52:56 +0500

    replaceTemplate Custom Module Created

--- index.js
+++ index.js
@@ -1,24 +1,14 @@
+// Core Modules
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+// Own Modules
+const replaceTemplate = require('./modules/replaceTemplate');
+
 ///////////////////////////////
 // SERVER
 ///////////////////////////////
-const replaceTemplate = (temp, product) => {
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
-
-    if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-
-    return output;
-}
 
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
@@ -58,6 +48,6 @@ const server = http.createServer((req, res) => {
     }
 });
 
-server.listen(8000, '127.0.0.1', () => {
+server.listen(8000, () => {
     console.log('Listening to requests on port 8000');
 });
